3f6d4bf277466953c18ca2336e54a6a7f78f16a2
OKBEntropy <cqpro.22@gmail.com>
2025-12-04 13:50:55 +0100

    V0.5-Body200VH

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Controle SMDSI</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <section1>
+
+        </section1>
+        <section2>
+
+        </section2>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
