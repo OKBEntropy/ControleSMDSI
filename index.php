776ab4538bc541752b8f5086f324d5679794a8b7
OKBEntropy <cqpro.22@gmail.com>
2025-12-04 14:03:20 +0100

    V0.6-Navbar

--- index.php
+++ index.php
@@ -4,14 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
     <title>Controle SMDSI</title>
 </head>
 
 <body>
+    <h1>CONTROLE DE SMDSI</h1>
     <header>
         <nav>
-
+            <ul id="nav-bar">
+                <li> <a href="#">Accueil</a> </li>
+                <li><a href="#Contact">Contactez-moi</a></li>
+            </ul>
         </nav>
     </header>
     <main>
